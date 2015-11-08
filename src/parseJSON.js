1a0f78dc5abdec77afaedc9a42a71a54c6f169d6
dmeidlin <git_hub@nym.hush.com>
2015-11-08 12:17:58 -0500

    Remove unnecessary parsedCollection = parsedCollection.replace(/\\\\/g,'\\');

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -12,88 +12,88 @@ regexp: true, indent: 2, maxerr: 50 */
 //};
 
 var parseableStrings = [
-  // //basic stuff
-  // '[]',
-  // '{"foo": ""}',
-  // '{}',
-  // '{"foo": "bar"}',
-  // '["one", "two"]',
-  // '{"a": "b", "c": "d"}',
-  // '[null,false,true]',
-  // '{"foo": true, "bar": false, "baz": null}',
-  // '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
-  // '{"boolean, true": true, "boolean, false": false, "null": null }',
+  //basic stuff
+  '[]',
+  '{"foo": ""}',
+  '{}',
+  '{"foo": "bar"}',
+  '["one", "two"]',
+  '{"a": "b", "c": "d"}',
+  '[null,false,true]',
+  '{"foo": true, "bar": false, "baz": null}',
+  '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
+  '{"boolean, true": true, "boolean, false": false, "null": null }',
 
-  // // basic nesting
-  // '{"a":{"b":"c"}}',
-  // '{"a":["b", "c"]}',
-  // '[{"a":"b"}, {"c":"d"}]',
-  // '{"a":[],"c": {}, "b": true}',
-  // '[[[["foo"]]]]',
+  // basic nesting
+  '{"a":{"b":"c"}}',
+  '{"a":["b", "c"]}',
+  '[{"a":"b"}, {"c":"d"}]',
+  '{"a":[],"c": {}, "b": true}',
+  '[[[["foo"]]]]',
 
   // escaping
   '["\\\\\\"\\"a\\""]',
-  '["and you can\'t escape thi\s"]'
+  '["and you can\'t escape thi\s"]',
 
-  // // everything all at once
-  // '{"CoreletAPIVersion":2,"CoreletType":"standalone",' +
-  //   '"documentation":"A corelet that provides the capability to upload' +
-  //   ' a folderâ€™s contents into a userâ€™s locker.","functions":[' +
-  //   '{"documentation":"Displays a dialog box that allows user to ' +
-  //   'select a folder on the local system.","name":' +
-  //   '"ShowBrowseDialog","parameters":[{"documentation":"The ' +
-  //   'callback function for results.","name":"callback","required":' +
-  //   'true,"type":"callback"}]},{"documentation":"Uploads all mp3 files' +
-  //   ' in the folder provided.","name":"UploadFolder","parameters":' +
-  //   '[{"documentation":"The path to upload mp3 files from."' +
-  //   ',"name":"path","required":true,"type":"string"},{"documentation":' +
-  //   ' "The callback function for progress.","name":"callback",' +
-  //   '"required":true,"type":"callback"}]},{"documentation":"Returns' +
-  //   ' the server name to the current locker service.",' +
-  //   '"name":"GetLockerService","parameters":[]},{"documentation":' +
-  //   '"Changes the name of the locker service.","name":"SetLockerSer' +
-  //   'vice","parameters":[{"documentation":"The value of the locker' +
-  //   ' service to set active.","name":"LockerService","required":true' +
-  //   ',"type":"string"}]},{"documentation":"Downloads locker files to' +
-  //   ' the suggested folder.","name":"DownloadFile","parameters":[{"' +
-  //   'documentation":"The origin path of the locker file.",' +
-  //   '"name":"path","required":true,"type":"string"},{"documentation"' +
-  //   ':"The Window destination path of the locker file.",' +
-  //   '"name":"destination","required":true,"type":"integer"},{"docum' +
-  //   'entation":"The callback function for progress.","name":' +
-  //   '"callback","required":true,"type":"callback"}]}],' +
-  //   '"name":"LockerUploader","version":{"major":0,' +
-  //   '"micro":1,"minor":0},"versionString":"0.0.1"}',
-  // '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
-  //   '25, "address" : { "streetAddress": "21 2nd Street", ' +
-  //   '"city" : "New York", "state" : "NY", "postalCode" : ' +
-  //   ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
-  //   '"number": "212 555-1234" }, { "type" : "fax", ' +
-  //   '"number": "646 555-4567" } ] }',
-  // '{\r\n' +
-  //   '          "glossary": {\n' +
-  //   '              "title": "example glossary",\n\r' +
-  //   '      \t\t"GlossDiv": {\r\n' +
-  //   '                  "title": "S",\r\n' +
-  //   '      \t\t\t"GlossList": {\r\n' +
-  //   '                      "GlossEntry": {\r\n' +
-  //   '                          "ID": "SGML",\r\n' +
-  //   '      \t\t\t\t\t"SortAs": "SGML",\r\n' +
-  //   '      \t\t\t\t\t"GlossTerm": "Standard Generalized ' +
-  //   'Markup Language",\r\n' +
-  //   '      \t\t\t\t\t"Acronym": "SGML",\r\n' +
-  //   '      \t\t\t\t\t"Abbrev": "ISO 8879:1986",\r\n' +
-  //   '      \t\t\t\t\t"GlossDef": {\r\n' +
-  //   '                              "para": "A meta-markup language,' +
-  //   ' used to create markup languages such as DocBook.",\r\n' +
-  //   '      \t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\r\n' +
-  //   '                          },\r\n' +
-  //   '      \t\t\t\t\t"GlossSee": "markup"\r\n' +
-  //   '                      }\r\n' +
-  //   '                  }\r\n' +
-  //   '              }\r\n' +
-  //   '          }\r\n' +
-  //   '      }\r\n'
+  // everything all at once
+  '{"CoreletAPIVersion":2,"CoreletType":"standalone",' +
+    '"documentation":"A corelet that provides the capability to upload' +
+    ' a folderâ€™s contents into a userâ€™s locker.","functions":[' +
+    '{"documentation":"Displays a dialog box that allows user to ' +
+    'select a folder on the local system.","name":' +
+    '"ShowBrowseDialog","parameters":[{"documentation":"The ' +
+    'callback function for results.","name":"callback","required":' +
+    'true,"type":"callback"}]},{"documentation":"Uploads all mp3 files' +
+    ' in the folder provided.","name":"UploadFolder","parameters":' +
+    '[{"documentation":"The path to upload mp3 files from."' +
+    ',"name":"path","required":true,"type":"string"},{"documentation":' +
+    ' "The callback function for progress.","name":"callback",' +
+    '"required":true,"type":"callback"}]},{"documentation":"Returns' +
+    ' the server name to the current locker service.",' +
+    '"name":"GetLockerService","parameters":[]},{"documentation":' +
+    '"Changes the name of the locker service.","name":"SetLockerSer' +
+    'vice","parameters":[{"documentation":"The value of the locker' +
+    ' service to set active.","name":"LockerService","required":true' +
+    ',"type":"string"}]},{"documentation":"Downloads locker files to' +
+    ' the suggested folder.","name":"DownloadFile","parameters":[{"' +
+    'documentation":"The origin path of the locker file.",' +
+    '"name":"path","required":true,"type":"string"},{"documentation"' +
+    ':"The Window destination path of the locker file.",' +
+    '"name":"destination","required":true,"type":"integer"},{"docum' +
+    'entation":"The callback function for progress.","name":' +
+    '"callback","required":true,"type":"callback"}]}],' +
+    '"name":"LockerUploader","version":{"major":0,' +
+    '"micro":1,"minor":0},"versionString":"0.0.1"}',
+  '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
+    '25, "address" : { "streetAddress": "21 2nd Street", ' +
+    '"city" : "New York", "state" : "NY", "postalCode" : ' +
+    ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
+    '"number": "212 555-1234" }, { "type" : "fax", ' +
+    '"number": "646 555-4567" } ] }',
+  '{\r\n' +
+    '          "glossary": {\n' +
+    '              "title": "example glossary",\n\r' +
+    '      \t\t"GlossDiv": {\r\n' +
+    '                  "title": "S",\r\n' +
+    '      \t\t\t"GlossList": {\r\n' +
+    '                      "GlossEntry": {\r\n' +
+    '                          "ID": "SGML",\r\n' +
+    '      \t\t\t\t\t"SortAs": "SGML",\r\n' +
+    '      \t\t\t\t\t"GlossTerm": "Standard Generalized ' +
+    'Markup Language",\r\n' +
+    '      \t\t\t\t\t"Acronym": "SGML",\r\n' +
+    '      \t\t\t\t\t"Abbrev": "ISO 8879:1986",\r\n' +
+    '      \t\t\t\t\t"GlossDef": {\r\n' +
+    '                              "para": "A meta-markup language,' +
+    ' used to create markup languages such as DocBook.",\r\n' +
+    '      \t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\r\n' +
+    '                          },\r\n' +
+    '      \t\t\t\t\t"GlossSee": "markup"\r\n' +
+    '                      }\r\n' +
+    '                  }\r\n' +
+    '              }\r\n' +
+    '          }\r\n' +
+    '      }\r\n'
 ];
 
 var unparseableStrings = [
@@ -144,7 +144,8 @@ var parseJSON = function (inString) {
   var splitUnparsedElements = function () {
     //remove the outer brackets.
     cleanUnparsedElements = cleanUnparsedElements.slice(1,-1);
-    cleanUnparsedElements = cleanUnparsedElements.replace(/\\\"/g,'"').replace(/\\/g,'\\');
+    cleanUnparsedElements = cleanUnparsedElements.replace(/\\"/g,'"');
+//    cleanUnparsedElements = cleanUnparsedElements.replace(/\\\"/g,'"').replace(/\\/g,'\\');
     //locate all of the commmas, but ignore any commas inside nested arrays, objects,
     //double quotes, or single quotes. 
     cursor.reset();
@@ -247,12 +248,13 @@ var parseJSON = function (inString) {
     //throw a SyntaxError if there are any unpaired brackets or quotes.
     try {
       if (cursor.doubleQuoteBalance > 0 || cursor.squareBracketBalance > 0 || cursor.curlyBraceBalance > 0) {
-        throw new SyntaxError("Unexpected end of input");
+        throw new SyntaxError("Unexpected end of input")
       }
     }
     catch (e) {
       console.log(e.message);
     }
+
     //parse the undefined, string, null, or boolean
     if (cleanUnparsedElements === 'true') {
       parsedCollection = true;
@@ -269,7 +271,7 @@ var parseJSON = function (inString) {
     //return a string if quotes are present at start and end.
     if (cleanUnparsedElements.charAt(0) === '"' && cleanUnparsedElements.charAt(cleanUnparsedElements.length - 1) === '"') {
       parsedCollection = cleanUnparsedElements.slice(1,-1);
-      parsedCollection = parsedCollection.replace(/\\\\/g,'\\');
+//      parsedCollection = parsedCollection.replace(/\\\\/g,'\\');
     }
     //check for a number by running parseFloat on each character and checking that
     //each result isFinite.
@@ -288,13 +290,13 @@ var i;
 for (i=0; i < parseableStrings.length; i++) {
   result[i] = parseJSON(parseableStrings[i]);
 }
-// for (i=0; i < unparseableStrings.length; i++) {
-//   result.push(parseJSON(unparseableStrings[i]));
-// }
+for (i=0; i < unparseableStrings.length; i++) {
+  result.push(parseJSON(unparseableStrings[i]));
+}
 for (i=0; i < parseableStrings.length; i++) {
   expected[i] = JSON.parse(parseableStrings[i]);
 }
-// for (i=0; i < unparseableStrings.length; i++) {
-//   expected.push(JSON.parse(unparseableStrings[i]));
-// }
+for (i=0; i < unparseableStrings.length; i++) {
+  expected.push(JSON.parse(unparseableStrings[i]));
+}
 
